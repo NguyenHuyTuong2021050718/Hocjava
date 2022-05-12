2ae1dc7da0123e2d0bdad30e2c90d14072a5b512
NguyenHuyTuong2021050718 <ngongtunho.2000@gmail.com>
2022-05-12 18:48:44 +0700

    comment

---
+++ lab4/src/Bài11/Main.java
@@ -0,0 +1,18 @@
+package Bài11;
+
+public class Main {
+	 
+    public static void main(String[] args) {
+        GVCoHuu gvch = new  GVCoHuu();
+        gvch.nhapthongtin();
+        gvch.inthongtin();
+        gvch.tinhluong();
+      
+       GVThinhGiang lgvtg = new GVThinhGiang();
+       lgvtg.nhapthongtin();
+       lgvtg.inthongtin();
+       lgvtg.tinhluong();
+      
+    }
+ 
+}
