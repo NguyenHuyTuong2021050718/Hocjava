42503be8466681ae0e94616cac2f29b8ca7194c6
NguyenHuyTuong2021050718 <ngongtunho.2000@gmail.com>
2022-05-12 17:54:34 +0700

    comment

---
+++ lab4/src/Bài10/HocVienDH.java
@@ -0,0 +1,55 @@
+package Bài10;
+
+public class HocVienDH extends HocVien {
+	public double HocPhi, DonGiaDH;
+	public int SoBuoi;
+
+	public void NhapHocVienDH() {
+		NhapThongTin();
+		@SuppressWarnings("unused")
+		HocVien LoaiUuTien;
+		System.out.println("Số Buổi : ");
+		SoBuoi = s.nextInt();
+		System.out.println("Đơn giá ĐH: ");
+		DonGiaDH = s.nextDouble();
+	}
+
+	public double getHocPhi() {
+		return HocPhi;
+	}
+
+	public void setHocPhi(double hocPhi) {
+		HocPhi = hocPhi;
+	}
+
+	public double getDonGiaDH() {
+		return DonGiaDH;
+	}
+
+	public void setDonGiaDH(double donGiaDH) {
+		DonGiaDH = donGiaDH;
+	}
+
+	public int getSoBuoi() {
+		return SoBuoi;
+	}
+
+	public void setSoBuoi(int soBuoi) {
+		SoBuoi = soBuoi;
+	}
+
+
+	public void HocPhi() {
+		System.out.println("Học Phí đồ họa :  ");
+			if (LoaiUuTien == 1) {
+				LoaiUuTien = 1000000;
+			} else if (LoaiUuTien == 2) {
+				LoaiUuTien = 500000;
+			} else {
+				System.err.println("Không có giá trị ưu tiên ! ");
+				return;
+			}
+			HocPhi = (SoBuoi * DonGiaDH) - LoaiUuTien;
+			System.out.println(HocPhi);
+	}
+}
